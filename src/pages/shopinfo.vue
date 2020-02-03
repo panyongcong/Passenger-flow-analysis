86b788c900eecf3209ef04a4597b86321079500c
panyongcong <984478031@qq.com>
2020-02-03 18:37:21 +0800

    map——two

--- src/pages/shopinfo.vue
+++ src/pages/shopinfo.vue
@@ -2,42 +2,53 @@
   <div class="shopinfo-main">
     <el-button type="primary" round>添加店面</el-button>
     <div class="allmap">
-      <baidu-map class="bm-view" ak="OSCnM1z1OLFZMMrqjevm77yy8fbYMxbT">
+      <baidu-map class="map" :center="map.center" :zoom="map.zoom" @ready="handler">
+        <!--缩放-->
+        <bm-navigation anchor="BMAP_ANCHOR_TOP_LEFT"></bm-navigation>
+        <!--定位-->
+        <bm-geolocation anchor="BMAP_ANCHOR_BOTTOM_RIGHT" :showAddressBar="true" :autoLocation="true"></bm-geolocation>
+        <!--点-->
+        <bm-marker :position="map.center" :dragging="map.dragging" animation="BMAP_ANIMATION_DROP">
+          <!--提示信息-->
+          <bm-info-window :show="map.show">Hello~</bm-info-window>
+        </bm-marker>
       </baidu-map>
     </div>
   </div>
 </template>
 <script>
 export default {
-  name: 'shopinfo'
+  name: 'shopinfo',
+  data: () => ({
+    map: {
+      center: {lng: 121.4472540000, lat: 31.3236200000},
+      zoom: 15,
+      show: true,
+      dragging: true
+    }
+  }),
+  methods: {
+    handler ({BMap, map}) {
+      let me = this
+      console.log(BMap, map)
+      // 鼠标缩放
+      map.enableScrollWheelZoom(true)
+      // 点击事件获取经纬度
+      map.addEventListener('click', function (e) {
+        console.log(e.point.lng, e.point.lat)
+      })
+    }
+  }
 }
 </script>
 <style>
   .allmap{
+    margin-top: 50px;
     width: 100%;
-    height: 300px;
+    height: 100%;
   }
-  .bm-view {
+  .map {
     width: 100%;
-    height: 300px;
-  }
-
-  h1,
-  h2 {
-    font-weight: normal;
-  }
-
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-
-  a {
-    color: #42b983;
+    height: 500px;
   }
 </style>
